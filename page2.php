8941ce48beb1e765dc5a1f809e45f4c8b2e955c5
BeauWilliams97 <beau.williams@my.jcu.edu.au>
2024-03-19 16:55:55 +1000

    created page1.php & page2.php / added both pages in inc_nav.php

---
+++ page2.php
@@ -0,0 +1,19 @@
+<?php include("dbconnect.php"); ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Page 2 - The Advice Shop</title>
+    <link href="styles/mainstyles.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+<?php include("inc_header.php"); ?>
+<?php include("inc_nav.php"); ?>
+<section id="content">
+    <h2>Welcome to Page 2</h2>
+    <p>This is the content of Page 2.</p>
+</section>
+<?php include("inc_footer.php"); ?>
+</body>
+</html>
