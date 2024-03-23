7848b1382694bd45c3d9e7c8bb7103ce70c1c272
BeauWilliams97 <beau.williams@my.jcu.edu.au>
2024-03-23 19:30:11 +1000

    linked login_db database to php through database.php,  created signup-success.php to display when a sign up is succesful, added signup form error checking in process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -1,28 +1,29 @@
 <?php
-/*Error checking to ensure name is entered when signing up.*/
+/*Error checking to ensure a name is entered when singing up*/
 if (empty($_POST["name"])) {
     die("Name is required");
 }
-/*Error checking to ensure a valid email is entered when singing up.*/
+/*Error checking to ensure a valid email is entered when signing up*/
 if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is required");
 }
-/*Error checking to ensure password is at least 8 characters long.*/
+/*Error checking to ensure account password is at least 8 characters long*/
 if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
-/*Error checking to ensure password contains at least 1 letter.*/
+/*Error checking to ensure password contains at least 1 letter*/
 if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
-/*Error checking to ensure password contains at least 1 number.*/
+/*Error checking to ensure password contains at least 1 number*/
 if ( ! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
-/*Error checking to ensure both passwords fields match.*/
+/*Error checking to ensure passwords match*/
 if ($_POST["password"] !== $_POST["password_confirmation"]) {
     die("Passwords must match");
 }
+/*Make it so password is encrypted when stored in db for security purposes*/
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
@@ -36,10 +37,28 @@ if ( ! $stmt->prepare($sql)) {
     die("SQL error: " . $mysqli->error);
 }
 
+$stmt->bind_param("sss",
+                  $_POST["name"],
+                  $_POST["email"],
+                  $password_hash);
+                  
+if ($stmt->execute()) {
+
+    header("Location: signup-success.php");
+    exit;
+    
+} else {
+    
+    if ($mysqli->errno === 1062) {
+        die("email already taken");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
 
 
 
 
 
-print_r($_POST);
-var_dump($password_hash);
+
+
---
+++ signup-success.php
@@ -0,0 +1,19 @@
+<?php include( "dbconnect.php" ); ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up - The Advice Shop</title>
+    <link href="styles/mainstyles.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+
+<body>
+<?php include( "inc_header.php" );
+include( "inc_nav.php" ); ?>
+<section id="content">
+    <h2>Signup Successful</h2>
+    <p style="text-align: center;"><strong>Welcome to the advice shop!</strong></p>
+</section>
+<?php include( "inc_footer.php" ); ?>
+</body>
+</html>
